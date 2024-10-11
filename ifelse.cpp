b936832eed42d6bb816c8146c26d8fceb6ea7087
Dakshata <dakshatamishralakshya@gmail.com>
2024-10-11 17:22:33 +0530

    added

---
+++ ifelse.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<< "Enter an integer";
+    cin>>x
+    if (x>0){cout<<"x is positive:" << x <<endl;}
+    else {cout<<"x is Negative :";}
+    
+}
